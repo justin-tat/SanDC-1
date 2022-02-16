7842b62046083f4daf60449d0b6f3b9303973192
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-02-15 18:57:28 -0800

    Refactored schemas

--- sdc/sdc-overview/sdc-dbs/reviewDb/index.js
+++ sdc/sdc-overview/sdc-dbs/reviewDb/index.js
@@ -6,33 +6,33 @@ mongoose.connect('mongodb://localhost/fetcher', { useNewUrlParser: true }, { use
 
 
 
-const review_photo_schema =
+const review_photo_schema = new mongoose.Schema(
 {
 
-  id: {type: Number, unique:true, required: true}
-  review_id: {type: Number, required: true}
+  id: {type: Number, unique:true, required: true},
+  review_id: {type: Number, required: true},
   url: {type: String, required: true}
-},
+});
 
 
-const reviewSchema = new Schema{
+const reviewSchema = new mongoose.Schema({
 
-  id: {type: Number,  unique: true, required: true  }
-  product_id: {type: Number,  unique: true, required: true  }
-  rating: {type: Number, required: true}
-  date: {type: String, required: true}
-  summary: {type: String, required: true}
-  body: {type: String, required: true}
-  recommend: {type: Boolean, required: true}
-  reported: {type: Boolean, required: true}
-  reviewer_name: {type: String, required: true}
-  reviewer_email: {type: String, required: true}
-  response: {type: String, required: true}
+  id: {type: Number, unique: true, required: true  },
+  product_id: {type: Number,  unique: true, required: true  },
+  rating: {type: Number, required: true},
+  date: {type: String, required: true},
+  summary: {type: String, required: true},
+  body: {type: String, required: true},
+  recommend: {type: Boolean, required: true},
+  reported: {type: Boolean, required: true},
+  reviewer_name: {type: String, required: true},
+  reviewer_email: {type: String, required: true},
+  response: {type: String, required: true},
   helpfulness: {type: Number, required: true}
-}
+})
 
 let Review = mongoose.model('Review', reviewSchema);
 let ReviewPhoto = mongoose.model('ReviewPhoto', review_photo_schema);
 
 
-//module.export.reviewSchema = reviewSchema;
+module.exports.Review = Review;
