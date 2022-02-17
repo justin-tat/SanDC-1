ae09f5056a64ccac68b6a1f3ddfb76687ba515ed
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-02-17 13:32:11 -0800

    Got example response at GET /meta/:product_id

--- Project-Atelier-main/server/RatingApi.js
+++ Project-Atelier-main/server/RatingApi.js
@@ -53,11 +53,14 @@ const updateReported = (reviewId) => {
 const ratingOverview = (productId) => {
   let options = {
     method: 'GET',
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews/meta?product_id=${productId}`,
+    //url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews/meta?product_id=${productId}`,
+    url: `http://localhost:3050/fec2/hr-rpp/reviews/meta/product_id=${productId}`,
+
     headers: { Authorization: gitToken.Token },
   };
   return axios(options)
     .then(response => {
+      console.log('this is response from meta', response.data);
       return response.data;
     })
     .catch((err) => {
