c52d11447c0af64b1a84d7e624a53dede7a8c839
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-02-17 21:01:54 -0800

    Moved all function definitions to helper file

--- sdc/routes/review.js
+++ sdc/routes/review.js
@@ -8,34 +8,11 @@ const helper = require('./reviewHelper.js');
 
 reviewRouter.get('/:product_id', async (req, res) => {
 
-  let receivedProductId = req.url.slice(1);
-  receivedProductId = 2;
-
-  function findDataInDb(productId){
-    return new Promise((resolve, reject)=>{
-      db.Review.find({product_id: receivedProductId}).limit().exec((err, data)=>{
-        if(err){
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      })
-    })
-  }
-  function findPhotoInDb(reviewId){
-    return new Promise((resolve, reject)=>{
-      db.ReviewPhoto.find({review_id:reviewId}).limit(5).exec((err, data)=>{
-        if(err){
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      })
-    })
-  }
+  let productId = req.url.slice(1);
+  productId = 2;
 
 
-  findDataInDb()
+  helper.findDataInDb(productId)
   .then(reviews => {
     let reviewId = [];
       for (var i = 0; i < reviews.length; i++){
@@ -45,7 +22,7 @@ reviewRouter.get('/:product_id', async (req, res) => {
       var promises = [];
         for(var i =0; i < reviewId.length; i++){
           let promise = new Promise ((resolve, reject)=>{
-            findPhotoInDb(reviewId[i])
+            helper.findPhotoInDb(reviewId[i])
            .then(data=>{
              resolve(data);
             })
@@ -81,211 +58,97 @@ reviewRouter.get('/:product_id', async (req, res) => {
 });
 
 reviewRouter.get('/meta/:product_id', async (req, res) => {
-  console.log('src router line 82');
-  // let answer = {
-  //     product_id: '64620',
-  //     ratings: { '1': '3', '4': '2', '5': '11' },
-  //     recommended: { false: '1', true: '15' },
-  //     characteristics: {
-  //       Fit: { id: 216798, value: '2.5714285714285714' },
-  //       Length: { id: 216799, value: '2.1428571428571429' },
-  //       Comfort: { id: 216800, value: '2.7142857142857143' },
-  //       Quality: { id: 216801, value: '2.5714285714285714' }
-  //     }
-  //   }
-  let id = req.url.slice(-5);
 
-    // function findRatingInDb(productId){
-    //   return new Promise((resolve, reject)=>{
-    //     db.Review.find({product_id: 1}).limit().exec((err, data)=>{
-    //       if(err){
-    //         reject(err);
-    //       } else {
-    //         resolve(data);
-    //       }
-    //     })
-    //   })
-    // }
+  let id = req.url.slice(-5);
 
     helper.findRatingInDb(id)
     .then(result => {
-      //console.log('found ratings in reviews coll', result);
       let resultAnswer = {
         product_id:id
       };
-
       let ratingVal = {}
       let recomObj = {};
       let falseCount = 0;
       let trueCount = 0;
-
-      for(var i =0; i < result.length; i++){
-        //find ratings
-        let review = result[i];
-        let rating = review.rating
-        if (ratingVal[rating] === undefined){
-          ratingVal[rating] = 1;
-        } else {
-          ratingVal[rating]++;
+        for(var i =0; i < result.length; i++){
+          //find ratings
+          let review = result[i];
+          let rating = review.rating
+            if (ratingVal[rating] === undefined){
+              ratingVal[rating] = 1;
+            } else {
+              ratingVal[rating]++;
+            }
+          //find recommendations
+          let recom = review.recommend;
+            if (recom === 'false'){
+              falseCount++;
+            } else {
+              trueCount++;
+            }
         }
-        //find recommendations
-       let recom = review.recommend;
-       if (recom === 'false'){
-         falseCount++;
-       }else {
-         trueCount++;
-       }
 
-
-      }
-     console.log ('ratingValline 155', ratingVal);
-     //{ '4': 1, '5': 1 }
-     for (var key in ratingVal){
-       ratingVal[key] = ratingVal[key].toString();
-
-     }
-     console.log ('ratingValline 161', ratingVal);
-
-
+        for (var key in ratingVal){
+          ratingVal[key] = ratingVal[key].toString();
+        }
       recomObj.false = falseCount.toString();
       recomObj.true = trueCount.toString();
 
-      //console.log(recomObj);
       resultAnswer.ratings = ratingVal;
       resultAnswer.recommended = recomObj;
-      //console.log(resultAnswer);
-      //found chars
 
-        findCharInDb(1)
-   .then(result => {
-    findReviewCharInDb(1).
-    then(reviewCharResult => {
-     //create char
+      //find chars
+      helper.findCharInDb(1)
+      .then(result => {
+         helper.findReviewCharInDb(1)
+        .then(reviewCharResult => {
+         //create char
+           let arr = [];
+           let characteristic_ids = new Set();
+            for (review of reviewCharResult) {
+              characteristic_ids.add(review['characteristic_id']);
+            }
+          // helper function to count average of array. [1,3] => 2
+            array_med = (x) => {
+              return x.reduce( (a,b) => {return a+b} ) / x.length ;
+             }
 
-let arr = [];
-let characteristic_ids = new Set();
-for (review of reviewCharResult) {
-  characteristic_ids.add(review['characteristic_id']);
-}
-console.log(51, characteristic_ids);
+            for (var i =0; i < reviewCharResult.length; i++){
+              let item = reviewCharResult[i];
+              let obj = {};
+              obj['id'] = item.characteristic_id;
+              obj['value']= item.value;
+               arr.push (obj);
+            }
+            var resArr = [];
 
-// helper function to count average of array. [1,3] => 2
-array_med = (x) => {
-  return x.reduce( (a,b) => {return a+b} ) / x.length ;
-}
-
-  for (var i =0; i < reviewCharResult.length; i++){
-    let item = reviewCharResult[i];
-    let obj = {};
-    obj['id'] = item.characteristic_id;
-    obj['value']= item.value;
-    arr.push (obj);
-  }
-
-  console.log(65, arr);
-  var resArr = [];
-
-  for (var i =0; i < arr.length; i++){
-    item = arr[i];
-    var t = resArr.findIndex(x => (x.id == item.id));
-    if(t <= -1){
-      item['value'] = array_med(arr.filter(x => (x.id == item.id) ).map(x=>x.value) ).toString();
-      resArr.push(item);
-      }
-  };
-console.log('line 191', resArr);
-let obj = {};
-for (var i =0; i < result.length; i++){
-  let item = result[i];
-  let name= item.name;
-  console.log(item.name);
-  obj[name] = {};
-  for (var j =0; j < resArr.length; j++){
-    let id = resArr[j].id;
-    if (id === item.id){
-      obj[name] = resArr[i];
-    }
-  }
-}
-
-console.log('line 206', obj);
-resultAnswer.characteristics = obj;
-console.log(resultAnswer);
-res.send(resultAnswer);
-
-
-
-
-
-
-
-    })
-     //console.log('find chars', result);
-
-     //finding char values by its id
-  //      findReviewCharInDb(1)
-  //  .then(result => {
-  //    console.log('found review chars', result);
-  //    console.log('line 168 charObj', charObj);
-  //    for(var i =0; i < result.length; i++){
-  //      for (var key in charObj){
-  //        let charReview = result[i];
-  //        if (charReview.characteristic_id === charObj.key[id]){
-  //          charObj.key[value] = charReview.value;
-  //        }
-  //      }
-  //    }
-  //    console.log('building char', charObj);
-
-  //  })
-
-   })
-
-    })
-
-
-
-
-    function findCharInDb(productId){
-      return new Promise((resolve, reject)=>{
-        db.Char.find({product_id:productId}).limit(5).exec((err, data)=>{
-          if(err){
-            reject(err);
-          } else {
-            resolve(data);
-          }
+            for (var i =0; i < arr.length; i++){
+              item = arr[i];
+              var t = resArr.findIndex(x => (x.id == item.id));
+              if(t <= -1){
+               item['value'] = array_med(arr.filter(x => (x.id == item.id) ).map(x=>x.value) ).toString();
+               resArr.push(item);
+               }
+            };
+            let obj = {};
+            for (var i =0; i < result.length; i++){
+              let item = result[i];
+              let name= item.name;
+              obj[name] = {};
+              for (var j =0; j < resArr.length; j++){
+                let id = resArr[j].id;
+                if (id === item.id){
+                 obj[name] = resArr[i];
+                }
+              }
+            }
+            resultAnswer.characteristics = obj;
+            console.log(resultAnswer);
+            res.send(resultAnswer);
         })
       })
-    }
-
-    function findReviewCharInDb(reviewId){
-      return new Promise((resolve, reject) => {
-        db.CharReview.find({review_id:reviewId}).exec((err, data)=>{
-          if(err){
-            reject(err);
-          } else {
-            resolve(data);
-          }
-        })
-      })
-    }
-
-  //  findCharInDb(1)
-  //  .then(result => {
-  //    console.log('find chars', result);
-  //  })
-
-  //  findReviewCharInDb(1)
-  //  .then(result => {
-  //    console.log('found review chars', result);
-  //  })
-
-    //res.send(answer);
+    })
 
 });
-
-
-
-
 
 module.exports = reviewRouter;
--- sdc/routes/reviewHelper.js
+++ sdc/routes/reviewHelper.js
@@ -4,7 +4,7 @@ const db = require('../sdc-overview/sdc-dbs/reviewDb/index.js');
 
 function findDataInDb(productId){
   return new Promise((resolve, reject)=>{
-    db.Review.find({product_id: receivedProductId}).limit().exec((err, data)=>{
+    db.Review.find({product_id: productId}).limit().exec((err, data)=>{
       if(err){
         reject(err);
       } else {
@@ -39,10 +39,36 @@ function findRatingInDb(productId){
   })
 }
 
+function findCharInDb(productId){
+  return new Promise((resolve, reject)=>{
+    db.Char.find({product_id:productId}).limit(5).exec((err, data)=>{
+      if(err){
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+function findReviewCharInDb(reviewId){
+  return new Promise((resolve, reject) => {
+    db.CharReview.find({review_id:reviewId}).exec((err, data)=>{
+      if(err){
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
 module.exports = {
   findDataInDb,
   findPhotoInDb,
-  findRatingInDb
+  findRatingInDb,
+  findCharInDb,
+  findReviewCharInDb
 }
 
 
