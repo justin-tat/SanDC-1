c7ae6a523522967004d11aca5bc4dda6748f89c2
Justin <jtat5912@gmail.com>
2022-03-09 19:20:33 -0800

    Changed connectionString in index.js to route to ec2 instance

--- sdc/sdc-overview/qnaDb/index.js
+++ sdc/sdc-overview/qnaDb/index.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 */
 
 
-const connectionString = 'postgres://postgres:Uncompromised@localhost:5432/questionsandanswers';
+const connectionString = 'postgres://postgres:Uncompromised@ec2-54-86-36-189.compute-1.amazonaws.com:5432/questionsandanswers';
 const db = new Client({connectionString: connectionString});
 db.connect(err => {
     if (err) {
