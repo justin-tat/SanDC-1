194a7827d78975a0d14ab9215f1f39188653d2dc
Justin <jtat5912@gmail.com>
2022-03-16 15:32:03 -0700

    Updated db connection

--- sdc/sdc-overview/qnaDb/index.js
+++ sdc/sdc-overview/qnaDb/index.js
@@ -11,8 +11,7 @@ const pool = new Pool({
 */
 
 //connectionString = 'dbType://username:password@address:port/database'
-var ip = 'ec2-52-91-81-110.compute-1.amazonaws.com';
-ip = "localhost";
+var ip = 'ec2-52-205-253-24.compute-1.amazonaws.com';
 const connectionString = `postgres://postgres:Uncompromised@${ip}:5432/questionsandanswers`;
 const db = new Client({connectionString: connectionString});
 db.connect(err => {
